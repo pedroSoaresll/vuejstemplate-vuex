36fd61debceaa23c202c3181216f391c43d7d9e0
pedroSoaresll <pedrodepaivasoaresll@gmail.com>
2018-05-08 17:15:51 -0300

    test: interface (andamento)

---
+++ test/unit/specs/pages/Interface.spec.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Component from 'vue-class-component'
+import Interface from '@/components/pages/Interface'
+
+
+describe('Interface.spec.js', () => {
+  let cmp, vm;
+
+  beforeEach(() => {
+    
+  })
+})
